1345c8e91364b9c53c8160d49a589fb4cd45a301
poole712 <54291199+poole712@users.noreply.github.com>
2024-04-03 18:21:39 +1300

    Start of segment manager editor

--- Assets/Scripts/S_SegmentManager.cs
+++ Assets/Scripts/S_SegmentManager.cs
@@ -5,9 +5,18 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+
+public class S_SegmentManagerAttribute : PropertyAttribute
+{
+    public string[] layer;
+}
 
 public class S_SegmentManager : MonoBehaviour
 {
+    [S_SegmentManager(layer = new string[] { "Layer 1 (Top)", "Layer 2", "Layer 3", "Layer 4", "Layer 5 (Bottom)" })]
+    public string specifiedLayer;
+
     [SerializeField]private List<GameObject> Segments;
     private List<GameObject> usedSegments; 
 
@@ -65,3 +74,99 @@ public class S_SegmentManager : MonoBehaviour
         }
     }
 }
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(S_SegmentManagerAttribute))]
+public class FloatPickerAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var attr = (S_SegmentManagerAttribute)attribute;
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        var propertyRect = new Rect(position.x, position.y, position.width - 20, position.height);
+        var dropdownButtonRect = new Rect(propertyRect.xMax, position.y, 20, position.height);
+
+        EditorGUI.EndProperty();
+
+        EditorGUI.PropertyField(propertyRect, property);
+
+        if (GUI.Button(dropdownButtonRect, "..."))
+        {
+            var menu = new GenericMenu();
+            foreach (var option in attr.layer)
+            {
+                menu.AddItem(new GUIContent(option.ToString()), false, () =>
+                {
+                    property.stringValue = option;
+                    property.serializedObject.ApplyModifiedProperties();
+                });
+            }
+            menu.ShowAsContext();
+        }
+
+    }
+}
+[CustomEditor(typeof(S_SegmentManager)), CanEditMultipleObjects]
+public class S_SegmentManagerEditor : Editor
+{
+    GUIStyle buttonStyle; 
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        serializedObject.ApplyModifiedProperties();
+
+        buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.normal.textColor = Color.white;
+        buttonStyle.fontSize = 15;
+
+        var layerProperty = serializedObject.FindProperty("specifiedLayer");
+        EditorGUILayout.PropertyField(layerProperty);
+
+        // Fetching specifiedLayer value using reflection
+        var targetObject = (S_SegmentManager)target;
+        var specifiedLayer = targetObject.specifiedLayer;
+
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            if (GUILayout.Button("Select all Specific Segments", buttonStyle))
+            {
+                var allEnemyBehaviour = GameObject.FindGameObjectsWithTag(specifiedLayer.ToString());
+                var allEnemyGameObjects = allEnemyBehaviour.Select(seg => seg.gameObject).ToArray();
+                Selection.objects = allEnemyGameObjects;
+            }
+
+            if (GUILayout.Button("Clear selection", buttonStyle))
+            {
+                Selection.objects = new UnityEngine.Object[0];
+            }
+
+
+        }
+
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            if (GUILayout.Button("Disable/Enable all Segments", buttonStyle))
+            {
+
+                foreach (var seg in GameObject.FindObjectsOfType<S_Segment>(true))
+                {
+                    Undo.RecordObject(seg.gameObject, "Disable/Enable segment");
+                    seg.gameObject.SetActive(!seg.gameObject.activeSelf);
+                }
+            }
+
+        }
+
+
+
+
+
+        }
+}
+
+
+#endif
