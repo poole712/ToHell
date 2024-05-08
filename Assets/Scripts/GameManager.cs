67fafd388c9d0b5912bc6e196752d4de76e2fd2e
Sean A. <seanagustin27@gmail.com>
2024-05-08 23:21:14 +1200

    Implemented a Pause/Play Button in the Game Level.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pauseMenu, pauseBtn, playBtn;
+    
+    public void OnEnable() 
+    {
+        Time.timeScale = 1;
+    }
+    
     public void Retry()
     {
         SceneManager.LoadScene(1);
@@ -14,5 +22,21 @@ public class GameManager : MonoBehaviour
     public void QuitToMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void ClickedPause()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        pauseBtn.SetActive(false);
+        playBtn.SetActive(true);    
+    }
+    
+    public void ClickedPlay()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false); 
+        pauseBtn.SetActive(true);
+        playBtn.SetActive(false);  
     }
 }
