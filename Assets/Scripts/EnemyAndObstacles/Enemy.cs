be8c9db0eb8eb331f93f1808dd9e4e1059fc171b
poole712 <54291199+poole712@users.noreply.github.com>
2024-05-25 11:30:25 +1200

    Added coin drops from killing enemies

--- Assets/Scripts/EnemyAndObstacles/Enemy.cs
+++ Assets/Scripts/EnemyAndObstacles/Enemy.cs
@@ -4,6 +4,7 @@ public class Enemy : GameEntities
 {
     public float Damage = 5.0f;  // Damage dealt by the enemy
     public ParticleSystem DestroyParticle;
+    public GameObject Coin;
 
     protected override void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,6 +21,13 @@ public class Enemy : GameEntities
         if (DestroyParticle != null)
         {
             Instantiate(DestroyParticle, transform.position, Quaternion.identity);
+            int cointAmount = Random.Range(0, 4);
+            for (int i = 0; i < cointAmount; i++)
+            {
+                GameObject coin = Instantiate(Coin, new Vector2(transform.position.x + 0.5f, transform.position.y + 0.5f), Quaternion.identity);
+                coin.GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(1, 5), Random.Range(2, 5)), ForceMode2D.Impulse);
+            }
+
         }
     }
 }
