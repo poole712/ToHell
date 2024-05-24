b0a0d701e492f9ba09d789f9598d62abff9ba137
Sean A. <seanagustin27@gmail.com>
2024-05-25 01:33:56 +1200

    Worked on displaying the shop. Also started on inventory mechanics

--- Assets/Scripts/Shop/ShopManager.cs
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class ShopManager : MonoBehaviour
 {
     public ShopItemSO[] shopItemSO;
-    public GameObject[] shopPanelsGO;
+    public GameObject[] shopPanelsGO, equipButtons, purchaseableUI, equippedUI;
     public ShopTemplate[] shopPanels;
     public Button[] purchaseButtons;
     public GameObject Shop;
     public CoinHandler CoinHandler;
     public SceneHandler SceneHandler;
+
+    int equippedCharacter = 2;
+    int equippedHammer = 1;
 
     // Start is called before the first frame update
     void OnEnable()
@@ -25,6 +28,7 @@ public class ShopManager : MonoBehaviour
     void Update()
     {
         CheckPurchaseable();
+        //CHECK EQUIPABLE
     }
 
     public void DisplayShop()
@@ -58,8 +62,10 @@ public class ShopManager : MonoBehaviour
         if (CoinHandler.GetCoins() >= shopItemSO[buttonN].basePrice)
         {
             CoinHandler.SubtractCoin(shopItemSO[buttonN].basePrice);
-            //REST OF IMPLEMENTATION
-
+            equipButtons[buttonN].SetActive(true);  
+            purchaseableUI[buttonN].SetActive(false);
+        
+            //SAVE ITEM AS EQUIPPABLE
         }
     }
 
@@ -77,6 +83,22 @@ public class ShopManager : MonoBehaviour
             shopPanels[i].title.text = shopItemSO[i].title;
             shopPanels[i].desc.text = shopItemSO[i].desc;
             shopPanels[i].basePrice.text = "$" + shopItemSO[i].basePrice.ToString();
+        }
+    }
+
+    public void EquipCharacterSkin(int buttonN) {
+        if(buttonN != equippedCharacter) {
+            equippedUI[equippedCharacter].SetActive(false);
+            equippedUI[buttonN].SetActive(true);
+            equippedCharacter = buttonN;
+        }
+    }
+
+    public void EquipHammerSkin(int buttonN) {
+        if(buttonN != equippedHammer) {
+            equippedUI[equippedHammer].SetActive(false);
+            equippedUI[buttonN].SetActive(true);
+            equippedHammer = buttonN;
         }
     }
 }
