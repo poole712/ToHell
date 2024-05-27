fb74627c72d4c45f1ad0f4aacd8219b317293a4e
poole712 <54291199+poole712@users.noreply.github.com>
2024-05-27 16:33:16 +1200

    Revert "Fix the unit test on Abilities"
    This reverts commit 5178dfd2971602b8a6e034bb7ab076fcb229aa83.

--- Assets/Tests/EditModeTests/Game.cs
+++ Assets/Tests/EditModeTests/Game.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-[Set Up]
 public class Game
 {
     private Random random = new Random();
-    private string[] pickupTypes = { "health_boost", "speed_boost", "invincibility" };
+    private string[] pickupTypes = { "health_boost", "speed_boost", "invincibility", "shield", "extra_points" };
     private Dictionary<int, List<string>> levelPickups = new Dictionary<int, List<string>>();
 
     public string[] GetPickupsForLevel(int level)
@@ -30,26 +29,5 @@ public class Game
         }
 
         return pickups.ToArray();
-    }
-}
-
-public class PickupTests
-{
-    [Test]
-    public void TestPickupVariationOnReplay()
-    {
-        var game = new Game();
-        var initialPickups = game.GetPickupsForLevel(1);
-        var replayPickups = game.GetPickupsForLevel(1);
-        CollectionAssert.AreNotEqual(initialPickups, replayPickups);
-    }
-
-    [Test]
-    public void TestPickupDiversityInSubsequentLevels()
-    {
-        var game = new Game();
-        var level1Pickups = game.GetPickupsForLevel(1);
-        var level2Pickups = game.GetPickupsForLevel(2);
-        CollectionAssert.AreNotEqual(level1Pickups, level2Pickups);
     }
 }
